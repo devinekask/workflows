1e1f4ab367d7451e76541e7f3d85ae54e11e31f0
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2022-12-06 14:30:34 +0100

    changed edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,7 +40,7 @@ const config = {
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
 					editUrl:
-						'https://github.com/devinekask/workflows/',
+						'https://github.com/devinekask/workflows/blob/main/',
 				},
 				blog: false,
 				theme: {
