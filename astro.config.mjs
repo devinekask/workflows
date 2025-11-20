f54e72884a8b94d32e92c4a75bdc49ff3577546c
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2025-11-20 09:58:41 +0100

    last updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import { remarkModifiedTime } from "./src/plugins/remark-modified-time.mjs";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://devinekask.github.io/",
@@ -9,6 +11,7 @@ export default defineConfig({
       title: "Workflows",
       favicon: "/favicon.ico",
       customCss: ["./src/styles/custom.css"],
+      lastUpdated: true,
       editLink: {
         baseUrl: "https://github.com/devinekask/workflows/edit/main/",
       },
